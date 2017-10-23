d8a7d02baca0d1150963bb6acbcb0eceec96d4dc
Sam Szreter <s.szreter@gmail.com>
2017-10-23 21:47:15 +0100

    Add Tsp._distance_matrix

--- test_tsp.py
+++ test_tsp.py
@@ -1,3 +1,24 @@
 import pytest
 
+import editdistance
 
+from main import Word
+from tsp import Tsp
+
+tsp = Tsp()
+word1 = Word('apple', 'sample')
+word2 = Word('orange', 'sample')
+word3 = Word('banana', 'sample')
+word4 = Word('pear', 'sample')
+words = [word1, word2, word3, word4]
+
+def test_it_compares_two_words_by_word_with_distance_function():
+    distance = tsp._compare(word1, word2, editdistance.eval)
+    assert distance == 5
+
+def test_it_makes_distance_matrix():
+    matrix = tsp._distance_matrix(words, editdistance.eval)
+    assert len(matrix) == 4
+    assert len(matrix[0]) == 4
+    assert matrix[1][1] == 0
+    assert matrix[0][1] == 5
--- tsp.py
+++ tsp.py
@@ -2,5 +2,16 @@ class Tsp():
     def __init__(self):
         return
 
-    def solve(words, distance_function):
-        return
+    def solve(self, words, distance_function):
+        matrix = self.distance_matrix(words, distance_function)
+
+    def _compare(self, word1, word2, distance_function):
+        return distance_function(word1.word, word2.word)
+
+    def _distance_matrix(self, words, distance_function):
+        l = len(words)
+        matrix = [[None for x in range(l)] for x in range(l)]
+        for i, n in enumerate(words):
+            for j, m in enumerate(words):
+                matrix[i][j] = self._compare(n, m, distance_function)
+        return matrix
